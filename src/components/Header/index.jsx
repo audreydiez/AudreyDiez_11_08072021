f07dce89778f1ab576b40beebc3a1e180ccb4406
Audrey Diez <audrey.diez@gmail.com>
2021-07-09 16:55:26 +0200

    init: clean files and add Linter, Prettier, babel and init component Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,17 @@
+import { Component } from 'react'
+
+class Header extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    render() {
+
+        return (
+            <div>Header</div>
+        )
+    }
+}
+
+export default Header
