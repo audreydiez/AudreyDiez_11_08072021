7d3ef87b422e096bf1f21cabe8bf35d69b1c6e2a
Audrey Diez <audrey.diez@gmail.com>
2021-07-15 16:28:59 +0200

    feat: add images and styles to RoomCard component
    fixes #10

--- src/components/RoomCard/index.jsx
+++ src/components/RoomCard/index.jsx
@@ -10,6 +10,7 @@ class RoomCard extends Component {
   render() {
     return (
       <article className="article">
+        <img className="article__image" src={this.props.apartment.cover} alt=""/>
         <div className="article__title">{this.props.apartment.title}</div>
       </article>
     )
