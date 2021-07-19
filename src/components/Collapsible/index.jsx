f2af5f6d678a5c1761addca8980b3b798ea4454b
Audrey Diez <audrey.diez@gmail.com>
2021-07-19 17:56:50 +0200

    feat: create component Collapse
    fixes #9

--- src/components/Collapsible/index.jsx
+++ src/components/Collapsible/index.jsx
@@ -1,13 +1,35 @@
 import { Component } from 'react'
+import './index.scss'
+
+import arrow from './../../assets/img/arrow.svg'
 
 class Collapsible extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      isOpen: false,
+    };
+
   }
 
+  handleClick = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
+
   render() {
-    return <div>Un composant collapsible</div>
+    const { isOpen } = this.state;
+
+    return <div className="collapsible">
+      <div className={`collapsible__title ${isOpen ? "is-open" : "is-close"}`} onClick={this.handleClick}>
+        <p dangerouslySetInnerHTML={{__html: this.state.isOpen}}>
+
+        </p>
+        <img src={arrow} alt=""/>
+      </div>
+      <div className={`collapsible__content ${isOpen ? "is-open" : "is-close"}`}>
+        <p>Content</p>
+      </div>
+    </div>
   }
 }
 
