7795a8f287b4a89ba6476745d23b2fe5ab61b064
Audrey Diez <audrey.diez@gmail.com>
2021-07-20 23:39:03 +0200

    feat: page About created
    closes #8

--- src/views/About/index.jsx
+++ src/views/About/index.jsx
@@ -1,23 +1,33 @@
 import { Component } from 'react'
 import Banner from '../../components/Banner'
 import Collapsible from '../../components/Collapsible'
+import './index.scss'
 
 import about from '../../assets/img/about.jpeg'
+import aboutData from './../../assets/data/about.json'
 
 class About extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
-  }
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
 
-  render() {
-    return (
-      <div>
-        <Banner picture={about} key="{about}" />
-        <Collapsible />
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <Banner picture={about} key="{about}" />
+                <div className="wrapper-about">
+                    {aboutData.map((data) => (
+                        <Collapsible
+                            title={data.title}
+                            content={data.content}
+                            key={data.id}
+                        />
+                    ))}
+                </div>
+            </div>
+        )
+    }
 }
 
 export default About
