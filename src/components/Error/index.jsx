f0571eac3f2938c1783334e91acdba7f6b6f17b5
Audrey Diez <audrey.diez@gmail.com>
2021-07-20 23:23:32 +0200

    feat: create  component Error
    closes #16

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,14 +1,31 @@
 import { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+import './index.scss'
 
 class Error extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
-  }
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
 
-  render() {
-    return <div>Failed !</div>
-  }
+    render() {
+        return (
+            <div className="wrapper-error">
+                <div className="wrapper-error__number">404</div>
+                <div className="wrapper-error__msg">
+                    Oups! La page que vous demandez n'existe pas.
+                </div>
+                <Link
+                    className="wrapper-error__back-link"
+                    to="/"
+                    title="Retour page d'accueil"
+                >
+                    Retourner sur la page d'accueil
+                </Link>
+            </div>
+        )
+    }
 }
 
 export default Error
--- src/components/Star/index.jsx
+++ src/components/Star/index.jsx
@@ -2,18 +2,17 @@ import * as React from 'react'
 import './index.scss'
 
 const Star = ({ fill, className }) => (
-
-<svg
-     viewBox="0 0 30 30"
-     fill={fill}
-     xmlns="http://www.w3.org/2000/svg"
-     className={'star-icon ${className}'}
->
-    <path d="M18.645 12L15 0L11.355 12H0L9.27 18.615L5.745 30L15 22.965L24.27 30L20.745 18.615L30 12H18.645Z"
-          fill={fill}
-    />
-</svg>
-
+    <svg
+        viewBox="0 0 30 30"
+        fill={fill}
+        xmlns="http://www.w3.org/2000/svg"
+        className={'star-icon ${className}'}
+    >
+        <path
+            d="M18.645 12L15 0L11.355 12H0L9.27 18.615L5.745 30L15 22.965L24.27 30L20.745 18.615L30 12H18.645Z"
+            fill={fill}
+        />
+    </svg>
 )
 
 export default Star
