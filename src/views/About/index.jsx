02002a8dd2c3db352bafd79e7e3b95eca480588c
Audrey Diez <audrey.diez@gmail.com>
2021-08-02 11:56:52 +0200

    clean: component About with function in render

--- src/views/About/index.jsx
+++ src/views/About/index.jsx
@@ -7,19 +7,33 @@ import about from '../../assets/img/about.jpeg'
 import aboutData from './../../assets/data/about.json'
 
 class About extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            aboutData: [],
+        }
+    }
+
+    componentDidMount() {
+        this.setState({ aboutData })
+    }
+
+    getAbout = () => {
+        return this.state.aboutData.map((data) => (
+            <Collapsible
+                title={data.title}
+                content={data.content}
+                key={data.id}
+            />
+        ))
+    }
+
     render() {
         return (
             <div>
                 <Banner picture={about} />
-                <div className="wrapper-about">
-                    {aboutData.map((data) => (
-                        <Collapsible
-                            title={data.title}
-                            content={data.content}
-                            key={data.id}
-                        />
-                    ))}
-                </div>
+                <div className="wrapper-about">{this.getAbout()}</div>
             </div>
         )
     }
